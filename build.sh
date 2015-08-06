0dfa4acf6cf390116a3c8440c6161b9b1d199297
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-08-06 14:31:43 -0300

    add git commit/push step

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
 cp ./src/* ./build/
+
+git remote add upstream "https://$GH_TOKEN@github.com/bcardiff/travis-hook-test"
+git fetch upstream
+git reset upstream/master
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:master
