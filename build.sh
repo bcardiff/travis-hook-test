087bf4a835e1a65572eaf0e12bf559536e23bfa5
Brian J. Cardiff <bcardiff@manas.com.ar>
2015-08-06 14:35:15 -0300

    git config for travis

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 cp ./src/* ./build/
 
+git config --global user.email "bcardiff@manas.com.ar"
+git config --global user.name "Travis on behalf Brian J. Cardiff"
+
 git remote add upstream "https://$GH_TOKEN@github.com/bcardiff/travis-hook-test"
 git fetch upstream
 git reset upstream/master
